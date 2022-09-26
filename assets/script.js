6b346be09e459df5de22225116815190f00cbce4
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-09-26 18:46:17 -0500

    added a date

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+{
+
+   
+    $('.datepicker').pickadate({
+        selectMonths: true, // Enable Month Selection
+        selectYears: 10, // Creates a dropdown of 10 years to control year
+        format:'dd/mm/yyyy'
+      });
+ 
+
+
+
+
+}
